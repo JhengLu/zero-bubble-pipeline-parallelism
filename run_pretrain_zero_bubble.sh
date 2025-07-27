e3f1c993006a9f020801aa0006a254d9800c2b23
JhengLu <jiahengluuu@gmail.com>
2025-07-27 16:53:20 +0000

    update the script

--- run_pretrain_zero_bubble.sh
+++ run_pretrain_zero_bubble.sh
@@ -6,7 +6,7 @@ rm -f gpu_log.csv
 # Start GPU logging in background and record its PID
 ./log_gpu.sh &
 LOG_PID=$!
-ZERO_BUBBLE_V_SCHEDULE=1 
+export ZERO_BUBBLE_V_SCHEDULE=1
 # Run training
 ./pretrain_zero_bubble.sh >> train_zero_bubble.log 2>&1
 
@@ -35,3 +35,4 @@ mkdir -p "$RESULT_DIR"
 cp gpu_log.csv pretrain_zero_bubble.sh train_zero_bubble.log run_pretrain_zero_bubble.sh "$RESULT_DIR"
 
 python data/zero_bubble_result/plot_memory.py  $RESULT_DIR
+python data/zero_bubble_result/plot_multiple_info.py  $RESULT_DIR
